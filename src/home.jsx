3eea4f7feb26615428b2b8c823365a838a28fe54
Sonia <sonia@192-168-1-109.tpgi.com.au>
2023-03-14 16:02:42 +1100

    add scroll detection for page bar

--- src/home.jsx
+++ src/home.jsx
@@ -7,42 +7,73 @@ const send = document.getElementById("send-button");
 window.onbeforeunload = function () {
     window.scrollTo(0, 0)
 }
-home.addEventListener('click', ()=> {
+function homeButton() {
     document.getElementById('home-line').style.display = 'block';
 
     document.getElementById('about-line').style.display = 'none';
     document.getElementById('projects-line').style.display = 'none';
     document.getElementById('contact-line').style.display = 'none';
+}
+function aboutButton() {
+    document.getElementById('about-line').style.display = 'block';
+
+    document.getElementById('home-line').style.display = 'none';
+    document.getElementById('projects-line').style.display = 'none';
+    document.getElementById('contact-line').style.display = 'none';
+}
+function projectsButton() {
+    document.getElementById('projects-line').style.display = 'block';
+
+    document.getElementById('home-line').style.display = 'none';
+    document.getElementById('about-line').style.display = 'none';
+    document.getElementById('contact-line').style.display = 'none';
+}
+function contactButton() {
+    document.getElementById('contact-line').style.display = 'block';
+
+    document.getElementById('home-line').style.display = 'none';
+    document.getElementById('about-line').style.display = 'none';
+    document.getElementById('projects-line').style.display = 'none';
+}
+home.addEventListener('click', ()=> {
+    homeButton();
     document.getElementById('home-content').scrollIntoView({behavior: 'smooth', block: 'center'});
 });
 
 about.addEventListener('click', ()=> {
-
-    document.getElementById('about-line').style.display = 'block';
+    aboutButton();
     document.getElementById('about-content').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'center'});
-
-    document.getElementById('home-line').style.display = 'none';
-    document.getElementById('projects-line').style.display = 'none';
-    document.getElementById('contact-line').style.display = 'none';
 });
 
 projects.addEventListener('click', ()=> {
-    document.getElementById('projects-line').style.display = 'block';
+    projectsButton();
     document.getElementById('projects-content').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'center'});
-
-    document.getElementById('home-line').style.display = 'none';
-    document.getElementById('about-line').style.display = 'none';
-    document.getElementById('contact-line').style.display = 'none';
 });
 
 contact.addEventListener('click', ()=> {
-    document.getElementById('contact-line').style.display = 'block';
+    contactButton();
     document.getElementById('contact-content').scrollIntoView({behavior: 'smooth', block: 'start', inline: 'center'});
-
-    document.getElementById('home-line').style.display = 'none';
-    document.getElementById('about-line').style.display = 'none';
-    document.getElementById('projects-line').style.display = 'none';
 });
+
+document.addEventListener('scroll', () => {
+    let homeContent = document.getElementById('home-content').getBoundingClientRect();
+    let abtContent = document.getElementById('about-content').getBoundingClientRect();
+    let pjtContent = document.getElementById('projects-content').getBoundingClientRect();
+    let conContent = document.getElementById('contact-content').getBoundingClientRect();
+    
+    if((homeContent.top >= 0 && homeContent.top + 200 <= window.innerHeight) || (homeContent.bottom >= 0 && homeContent.bottom <= window.innerHeight)){
+        homeButton();
+    }
+    if((abtContent.top >= 0 && abtContent.top + 200 <= window.innerHeight) || (abtContent.bottom >= 0 && abtContent.bottom <= window.innerHeight)){
+        aboutButton();
+    }
+    if((pjtContent.top >= 0 && pjtContent.top + 200 <= window.innerHeight) || (pjtContent.bottom >= 0 && pjtContent.bottom <= window.innerHeight)){
+        projectsButton();
+    }
+    if((conContent.top >= 0 && conContent.top + 200 <= window.innerHeight) || (conContent.bottom >= 0 && conContent.bottom <= window.innerHeight)){
+        contactButton();
+    }
+})
 
 send.addEventListener('click', () => {
     let sendTo = "sonia899998@gmail.com";
